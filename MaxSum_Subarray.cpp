e779153d7fd46f16ec990a342cd710b38c7fd390
Abhishek Chaudhary <86932418+akaabhishek@users.noreply.github.com>
2021-12-18 15:24:51 +0530

    Add files via upload
    MaxSum Subarray - This popular algorithm also known by the name of "KADANE'S ALGORITHM".
    This problems description goes as follows :
              We're given with an array/vector of integers(negative integers included) and we have to find  and print out the maximum sum from all the possible sub-arrays.
    Brute Force approach - Printing all the possible sub arrays and then find out the sub array with the maximum sum, this approach will get us with time complexity of O(n^3).
    The above solution do the same but within time complexity of O(n).

---
+++ MaxSum_Subarray.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<limits>
+using namespace std;
+int main()
+{
+    int maxsum=INT_MIN;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int current_sum=0;
+    for(int i=0;i<n;i++)
+    {
+        current_sum+=arr[i];    
+        
+        if(current_sum<1)
+        {
+            current_sum=0;
+        }
+
+        if(current_sum>maxsum)
+        {
+            maxsum=current_sum;
+        }
+    }
+    cout<<maxsum;
+return 0;
+}
